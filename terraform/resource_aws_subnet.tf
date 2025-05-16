7b7e228727f645793e1157127be3bc36c8fa841e
AJLab-GH <ajammes.ftnt@gmail.com>
2025-05-15 19:48:28 -0600

    adding iac to bot

--- terraform/resource_aws_subnet.tf
+++ terraform/resource_aws_subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true  # ❌ This causes public IPs to be assigned by default
   availability_zone       = "us-west-2a"
   tags = {
